ebce17454edd73a840a7d6041555b15ed673a8f5
Shayene <78311101+shaylcm@users.noreply.github.com>
2021-07-21 15:23:53 -0300

    correção de fórmula
    o resultado do desvio padrão estava dando errado, testei aqui em vários conversores, então fiquei curiosa para saber onde estava o erro e agora o resultado bate .Eram só alguns detalhes na formula, espero ter ajudado.

--- Capitulo 6/6.7.1/ex14.c
+++ Capitulo 6/6.7.1/ex14.c
@@ -3,17 +3,22 @@
 
 int main(){
     int i;
-    float vetor[10], media = 0, variancia = 0, d;
+    float vetor[10], media = 0, variancia = 0, d,MEDIA=0,VARIANCIA=0,VAS=0;
     printf("Digite os 10 numeros do vetor: ");
     
     for(i = 0; i < 10; i++){
         scanf("%f", &vetor[i]);
-        media = (media + vetor[i]) / 9;
+        media = (media + vetor[i]) 
     }
-    for(i = 0; i < 10; i++)
-        variancia = pow(vetor[i] - media, 2);
+    MEDIA=media/10;
     
-    d = sqrt(variancia);
+    for(i = 0; i < 10; i++)
+    {
+        variancia = pow(vetor[i] - MEDIA, 2);
+        VAS=VAS+variancia;
+    }
+    VARIANCIA=VAS/9;
+    d = sqrt(VARIANCIA);
     printf("Desvio padrao: %f\n", d);
     return 0;
 }
